41c462f1121a50e1ff4d6ca53b801d8312c0e40e
lizezhou@caicloud.io <lzzduke06@gmail.com>
2018-03-15 22:50:12 +0800

    add init gnn code with tf.estimator

--- initial_net.py
+++ initial_net.py
@@ -1,8 +1,12 @@
 import numpy as np
+import tensorflow as tf
+
+
+N = 4000
+STEPS = 5000
 
 
 if __name__ == '__main__':
-    N = 4000
     image = []
     label_tmp = []
 
@@ -13,8 +17,46 @@ if __name__ == '__main__':
         label_tmp.append(label[0, :])
         print(i)
 
-    x = image
-    t = label_tmp
+    x = np.array(image)
+    label = np.array(label_tmp)
 
-    print(x)
+    train_length = int(0.95*len(x))
 
+    train_x = x[:train_length]
+    train_label = label[:train_length]
+
+    test_x = x[train_length:]
+    test_label = label[train_length:]
+
+    hidden_layer_size = 50
+
+    # Specify that all features have real-value data
+    feature_columns = [tf.feature_column.numeric_column("x", shape=[75])]
+
+    # Build a DNNRegressor.
+    model = tf.estimator.DNNRegressor(label_dimension=3,
+                                      feature_columns=feature_columns,
+                                      hidden_units=[hidden_layer_size],
+                                      model_dir="init_model")
+
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_x},
+        y=train_label,
+        batch_size=128,
+        num_epochs=None,
+        shuffle=True)
+
+    test_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": test_x},
+        y=test_label,
+        batch_size=128,
+        num_epochs=1,
+        shuffle=False)
+
+    # Train the model.
+    model.train(input_fn=train_input_fn, steps=STEPS)
+
+    eval_result = model.evaluate(input_fn=test_input_fn)
+    average_loss = eval_result["average_loss"]
+
+    print(average_loss)
